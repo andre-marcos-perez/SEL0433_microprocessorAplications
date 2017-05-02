28dbbd8e7626dd9ebcd062343ad6f6bddcb05b8e
Andre Perez <andre_mperez@hotmail.com>
2017-05-02 12:31:18 -0300

    Update exercise2.asm

--- Exam_1-2010/exercise2.asm
+++ Exam_1-2010/exercise2.asm
@@ -1,1 +1,52 @@
+;*******************************************************************************
+; @INSTITUTION
+;  University of Sao Paulo | Sao Carlos School of Engineering | SEL
+;------------------------------------------------------------------------------
+; @DISCIPLINE
+;  Name: SEL0433 | Applications of Microprocessors I
+;  Professor: Evandro Luis Linhari Rodrigues
+;  Semester: 2017\01
+;------------------------------------------------------------------------------
+; @DEVELOPMENT
+;  MCU:	Intel 8052
+;  IDE: MCU 8051 v1.5.7
+;  Compiler: IDE native assembler
+;------------------------------------------------------------------------------
+; @WARRANTY
+;  Copyright (c) 2017 Andre Marcos Perez
+;  The software is provided by "as is", without warranty of any kind, express or
+;  implied, including but not limited to the warranties of merchantability,
+;  fitness for a particular purpose and noninfringement. In no event shall the
+;  authors or copyright holders be liable for any claim, damages or other
+;  liability, whether in an action of contract, tort or otherwise, arising from,
+;  out of or in connection with the software or the use or other dealings in the
+;  software.
+;------------------------------------------------------------------------------
+; @EXERCISE
+;  Exam: #1
+;  Ex. : #2
+;------------------------------------------------------------------------------
+; @AUTHOR
+;  Name:  Andre Marcos Perez
+;  Email: andre.marcos.perez@usp.br
+;  #USP:  8006891
+;*******************************************************************************
 
+;*******************************************************************************
+; @CODE
+;  Main code
+;------------------------------------------------------------------------------
+	ORG	0
+	MOV	R0,#08H
+LOOP:	JNB	20H.0,ROTATE
+	CPL	27H.7
+ROTATE:	MOV	A,20H
+	RL	A
+	MOV	20H,A
+	MOV	A,27H
+	RR	A
+	MOV	27H,A
+	DJNZ	R0,LOOP
+	SJMP	$
+;*******************************************************************************
+	END
